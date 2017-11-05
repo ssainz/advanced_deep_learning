ded999ad3fd4a35b1fa3f027f7133e8f7db19440
Sergio Sainz <sergio.sainz@gmail.com>
2017-11-05 00:05:15 -0400

    sequence to sequence

--- homeworkIII/sequence_learning.py
+++ homeworkIII/sequence_learning.py
@@ -61,7 +61,7 @@ class LSTM_CELL:
         return (self.h_t, self.C_t)
 
 
-def generate_seq_to_seq_network(size_of_input_x, size_of_output_y, size_of_h_t, time_steps, number_of_stacked_layers, number_of_warming_up_words):
+def generate_seq_to_seq_network(size_of_input_x, size_of_output_y, size_of_h_t, time_steps, number_of_stacked_layers):
 
     X = tf.placeholder(shape=(size_of_input_x, time_steps, None), dtype=tf.float32)
     Y_hat = tf.placeholder(shape=(size_of_output_y, time_steps, None), dtype=tf.float32)
@@ -71,8 +71,8 @@ def generate_seq_to_seq_network(size_of_input_x, size_of_output_y, size_of_h_t, 
 
     b_y = tf.Variable(tf.random_normal([size_of_output_y, 1], stddev=0.1), name="b_o")
 
-    C_0s = np.unstack(C_0, axis=0)
-    h_0s = np.unstack(h_0, axis=0)
+    C_0s = tf.unstack(C_0, axis=0) # each C_0s will be size [size_of_h_t, batch_size]
+    h_0s = tf.unstack(h_0, axis=0) # each h_0s will be size [size_of_h_t, batch_size]
 
     # Create LSTM cells
     lstm_cells = []
@@ -83,7 +83,7 @@ def generate_seq_to_seq_network(size_of_input_x, size_of_output_y, size_of_h_t, 
     x_slices_per_time_step = tf.unstack(X, axis=1)  # each slice will be of shape (size_of_input_x, batch_size)
 
     # unpack Y:
-    y_slices_per_time_step = tf.unstack(Y_hat, axis=1)  # each slice will be of shape (size_of_input_x, batch_size)
+    y_slices_per_time_step = tf.unstack(Y_hat, axis=1)  # each slice will be of shape (size_of_output_y, batch_size)
 
     # init losses array:
     losses = []
@@ -128,44 +128,65 @@ def generate_seq_to_seq_network(size_of_input_x, size_of_output_y, size_of_h_t, 
         for stack in range(number_of_stacked_layers):
             # Get the cell:
             lstm_cell = lstm_cells[stack]
-            sum = sum + tf.matmul(lstm_cell.W_out, lstm_cell.h_t)
+            sum = tf.add_n([tf.matmul(lstm_cell.W_out, lstm_cell.h_t), sum])
         # Here we must sum b_y to each row
-        # sum is (size_of_output_y, batches), b_y is (size_of_output_y, 1)
-        sum = tf.add(sum, b_y)
+        # predicted_y_before_softmax is (size_of_output_y, batches), b_y is (size_of_output_y, 1)
+        predicted_y_before_softmax = tf.add(sum, b_y)
 
-        predicted_y = tf.nn.softmax(sum, dim=0)
+        predicted_y = tf.nn.softmax(predicted_y_before_softmax, dim=0)
         predicted_ys.append(predicted_y)
 
-
-        probability_actual_given_prediction = tf.log(tf.multiply(predicted_y, y_slices_per_time_step[time_step]))
-        losses.append(probability_actual_given_prediction) # losses is a list of tensors, each tensor in this list is shape [size_of_output_y, batches]
+        # probability the actual next word is chosen given current prediction.
+        # predicted_y_before_softmax has shape (size_of_output_y, batches)
+        # y_slices_per_time_step[time_step] has shape (size_of_output_y, batches)
+        # cross_entropy_per_time_step_per_batches has shape [batch_size]
+        cross_entropy_per_batches = tf.nn.softmax_cross_entropy_with_logits(labels=y_slices_per_time_step[time_step], logits=predicted_y_before_softmax, dim=0)
+        # losses is a list of tensors, each tensor in this list is shape [size_of_output_y, batches]
+        losses.append(cross_entropy_per_batches)
 
         first_time_step = False
 
     # Here we just sum all samples (from all batches), over all the time steps.
-    # Should we first sumover the times and then average each sample ?
-    loss = -1 * tf.reduce_sum(losses)
+    # losses after the stack transformation will be shape [time_steps, batch_size]
+    losses_per_time_step_per_batch = tf.stack(losses, axis=0)
+    # losses_per_batch is shape [batch_size]
+    losses_per_batch = tf.reduce_sum(losses_per_time_step_per_batch, axis = 0)
+    # now average over the batches:
+    loss = tf.reduce_mean(losses_per_batch, axis=0)
 
     # Create an optimizer.
     eta = 0.0001
-    opt = tf.AdamOptimizer(learning_rate=eta)
+    opt = tf.train.AdamOptimizer(learning_rate=eta)
 
     # Compute the gradients for a list of variables.
     grads_and_vars = opt.compute_gradients(loss)
 
     # grads_and_vars is a list of tuples (gradient, variable).  Do whatever you
     # need to the 'gradient' part, for example cap them, etc.
-    capped_grads_and_vars = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in grads_and_vars]
+    capped_grads_and_vars = []
+    for grad, var in grads_and_vars:
+        if grad is not None:
+            grad_ = tf.clip_by_value(grad, -1., 1.)
+        else:
+            grad_ = tf.zeros_like(var)
+        capped_grads_and_vars.append((grad_,var))
+
+    # Below is breaking due to https://github.com/tensorflow/tensorflow/issues/783
+    # capped_grads_and_vars = [(tf.clip_by_value(grad, -1., 1.), var ) for grad, var in grads_and_vars ]
 
     # Ask the optimizer to apply the capped gradients.
     train = opt.apply_gradients(capped_grads_and_vars)
 
     # Accuracy:
+    # stacked_predicted_ys is shape [time_step, size_of_output_y, batch_size]
     stacked_predicted_ys = tf.stack(predicted_ys, axis=0)
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(Y_hat, 1))
+    # adjust stacked_predicted_ys to [size_of_output_y, time_step, batch_size]
+    stacked_predicted_ys = tf.transpose(stacked_predicted_ys, perm=[1,0,2])
+    # Y_hat is shape [size_of_output_y, time_step, batch_size]
+    correct_prediction = tf.equal(tf.argmax(stacked_predicted_ys, 0), tf.argmax(Y_hat, 0))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    return X, Y_hat, C_0, h_0, train, predicted_ys, loss, accuracy
+    return X, Y_hat, C_0, h_0, train, stacked_predicted_ys, predicted_ys, loss, accuracy
 
 
 def generate_words_one_hot_table(sorted_words_list):
@@ -200,7 +221,13 @@ def read_words(file_name):
 
     words_one_hot_table = generate_words_one_hot_table(words_sorted)
 
-    return words_one_hot_table
+    i = 0
+    index_to_word_table = {}
+    for word in words_sorted:
+        index_to_word_table[i] = word
+        i += 1
+
+    return words_one_hot_table, index_to_word_table
 
 
 def read_words_sequence(file_name, words_table):
@@ -276,9 +303,34 @@ def generate_X_and_Y_hat_for_sequence(sequence):
     return np.transpose(X_, (1,0,2)), np.transpose(Y_, (1,0,2))
 
 
+def one_hot_series_to_sentences(sequences_one_hot_vectors, index_to_word_table):
+    # sequences_one_hot_vectors shape is [size_of_x, time_steps, number_of_batches]
+    # transpose to batches as first dimensions, then time steps, then size of x
+
+    sequences = np.transpose(sequences_one_hot_vectors, (2,1,0))
+    result = ""
+    first = True
+    for sentence in sequences:
+
+        if first == True:
+            first = False
+        else:
+            result = result + "\n"
+
+        for one_hot_word in sentence:
+            index = np.argmax(one_hot_word)
+            word_str = index_to_word_table[index]
+            result = result + " " + word_str
+
+    return result
+
+
+
+
+
 # First reads the words into one-hot vectors:
 file_name = "/srv/datasets/sequence_to_sequence/text_file.txt"
-words_table = read_words(file_name)
+words_table , index_to_words_table = read_words(file_name)
 
 # print('---')
 # for key in words_table:
@@ -307,19 +359,27 @@ size_of_output_y = 768
 size_of_h_t = 512
 time_steps = num_of_words_before_prediction_N + number_of_words_to_be_predicted
 number_of_stacked_layers = 3
-(X, Y_hat, C_0, h_0, train, predicted_ys, loss, accuracy) = generate_seq_to_seq_network(size_of_input_x,
+(X, Y_hat, C_0, h_0, train, stacked_predicted_ys, predicted_ys, loss, accuracy) = generate_seq_to_seq_network(size_of_input_x,
                                                                                         size_of_output_y,
                                                                                         size_of_h_t,
                                                                                         time_steps,
                                                                                         number_of_stacked_layers)
+
+# Save model for later :)
+saver = tf.train.Saver()
 
 # Start training:
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+# In case we want to restore previous model, just use:
+# 1) iterations set to 0
+# 2) restore:
+# saver.restore(sess, "/srv/datasets/sequence_to_sequence/model.ckpt")
+# print("model restored")
 
-iterations = 1
+iterations = 10
 # One iteration is considered seeing all the potential sequences at least once in random order.
 for it in range(iterations):
     # Generate minibatches of sequences:
@@ -327,6 +387,7 @@ for it in range(iterations):
                                                         number_of_words_to_be_predicted)
 
     # For each minibatch
+    j = 0
     for minibatch in list_of_batches:
 
         # Generate X and Y:
@@ -339,8 +400,16 @@ for it in range(iterations):
         h_0_ = np.zeros(shape=(number_of_stacked_layers, size_of_h_t, batch_size), dtype=np.float32)
 
         # Train:
-        (cur_train, cur_loss, cur_accuracy, cur_predicted_ys) = sess.run([train, loss, accuracy, predicted_ys],
+        (cur_train, cur_loss, cur_accuracy, cur_stacked_predicted_ys) = sess.run([train, loss, accuracy, stacked_predicted_ys],
                                                                                   {X: X_, Y_hat: Y_hat_, h_0: h_0_, C_0: C_0_})
+
+        if (j + 1) % 10  == 0:
+            print("it (%s)(%s), loss(%s), accuracy (%s)" % (it, j, cur_loss, cur_accuracy))
+
+        j += 1
+
+        # For early stopping
+        # break
 
     if (it + 1) % 5000 == 0:
 
@@ -350,7 +419,72 @@ for it in range(iterations):
 print("it (%s), loss(%s), accuracy (%s)" % (it, cur_loss, cur_accuracy) )
 
 
+save_path = saver.save(sess, "/srv/datasets/sequence_to_sequence/model.ckpt")
+print("Model saved in file: %s" % save_path)
+
 # Evaluation
+
+# First generate one sequence:
+test_batch_size = 2
+list_of_batches = generate_random_sequences_batches(training_sequence, test_batch_size, num_of_words_before_prediction_N,
+                                                        number_of_words_to_be_predicted)
+
+# first batch
+test_sample = list_of_batches[0]
+
+# Generate X and Y:
+test_X_, test_Y_hat_ = generate_X_and_Y_hat_for_sequence(test_sample)
+
+# Now the interesting part, we will find the forecasted y words:
+# 1) We will cut the last 50 words of the X_ sequence and replace with zeros.
+# 2) Then, we will input Y_hat_, X_ and calculate 50 times the next word ith y position (when i moves from 1 to 50 next words)
+# We will update X_ with each new word.
+# 3) we will extract those values and convert to string, then print the result and compare with actual.
+
+# 1) We will cut the last 50 words of the X_ sequence and replace with zeros.
+# test_X is shape [size_of_input_x, time_steps, batch_size]
+test_X_[:,num_of_words_before_prediction_N:,:] = 0.0
+
+test_C_0_ = np.zeros(shape=(number_of_stacked_layers, size_of_h_t, test_batch_size), dtype=np.float32)
+test_h_0_ = np.zeros(shape=(number_of_stacked_layers, size_of_h_t, test_batch_size), dtype=np.float32)
+
+# Initial word:
+print("Model evaluation, pick random sentence(s), first %s words:" % num_of_words_before_prediction_N)
+print(one_hot_series_to_sentences(test_X_[:,0:num_of_words_before_prediction_N,:], index_to_word_table=index_to_words_table))
+
+# 2) Then, we will input Y_hat_, X_ and calculate 50 times the next word ith y position (when i moves from 1 to 50 next words)
+# We will update X_ with each new word.
+for i in range(number_of_words_to_be_predicted):
+    index_of_next_word_to_predict = i + num_of_words_before_prediction_N
+    (cur_loss, cur_accuracy, cur_word_prediction) = sess.run([loss, accuracy, predicted_ys[index_of_next_word_to_predict]], {X: test_X_, Y_hat: test_Y_hat_, h_0: test_h_0_, C_0: test_C_0_})
+
+    # Find new word and append it to the test_X_
+    # Shape of cur_word_prediction is (size_of_output_y, batches)
+    index_per_batches = np.argmax(cur_word_prediction, axis=0)
+    new_slice = []
+    for index in index_per_batches:
+        new_word_one_hot = np.zeros(size_of_input_x)
+        new_word_one_hot[index] = 1.0
+        new_slice.append(new_word_one_hot)
+    new_slice = np.stack(new_slice, axis=0)
+    # final shape of new slice after transpose is (size_of_input_x,batch_size)
+    new_slice = np.transpose(new_slice, (1,0))
+
+    if i + 1 != number_of_words_to_be_predicted:
+        test_X_[:,index_of_next_word_to_predict + 1,:] = new_slice
+
+print("Model evaluation, random sentence(s), predicted words:")
+print(one_hot_series_to_sentences(test_X_,index_to_word_table=index_to_words_table))
+
+print("Model evaluation, random sentence(s), expected words:")
+print(one_hot_series_to_sentences(test_Y_hat_,index_to_word_table=index_to_words_table))
+
+
+
+
+
+
+
 
 
 
